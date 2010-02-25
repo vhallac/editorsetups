5bfc94b2bcea256268732e73fd6212c9c30a5db0
Vedat Hallac <vedathallac@gmail.com>
2010-02-25 11:57:05 +0200

    windowed: Set cursor color
    The cursor color is black on windows and white on linux. Disgusting.

--- .emacs.common/config.el
+++ .emacs.common/config.el
@@ -47,6 +47,7 @@
         (setq initial-frame-alist (append '((top . 20) (left . 1))
                                           (if (>= (x-display-pixel-height) 1024)
                                               (list (cons 'height 55))))))
+       (set-cursor-color "light green")
        (GNULinux
         (if (>= (x-display-pixel-height) 1024)
             (setq initial-frame-alist '((height . 55))))))))
