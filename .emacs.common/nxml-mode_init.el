a272c0c1e87d5e808689e0c314171281ff7cbd07
Vedat Hallac <vedathallac@gmail.com>
2010-04-12 16:18:24 +0300

    nxml-mode: Remove from custom packages.
    Use the nxml-mode provided by emacs23.

--- .emacs.common/nxml-mode_init.el
+++ .emacs.common/nxml-mode_init.el
@@ -1,7 +1,7 @@
 ;;--
 ;; Make sure nxml-mode can autoload
 ;;--
-(load "rng-auto.el")
+;;(load "rng-auto.el")
 
 ;;--
 ;; Load nxml-mode for files ending in .xml, .xsl, .rng, .xhtml
@@ -10,5 +10,6 @@
              '("\\.\\(xml\\|xsl\\|rng\\|xhtml\\|html\\|htm\\)\\'" . nxml-mode))
 
 (custom-set-variables
- '(nxml-slash-auto-complete-flag t))
+ '(nxml-slash-auto-complete-flag t)
+ '(rng-schema-locating-files (quote ("schemas.xml" "~/.emacs.common/data/schema/schemas.xml"))))
 (custom-set-faces)
