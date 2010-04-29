bbd70e19ca12dcfafff723ded324a46f3925b5c2
Vedat Hallac <vedathallac@gmail.com>
2010-04-29 13:26:56 +0300

    python-mode: Get rid of the old python package
    I will not use the better supported python package from emacs23.1

--- .emacs.common/python-mode_init.el
+++ .emacs.common/python-mode_init.el
@@ -1,7 +1,21 @@
+(require 'python)
+
 (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 (autoload 'python-mode "python-mode" "Python editing mode." t)
 
-(custom-set-variables
- '(py-python-command "d:\\python24\\python.exe"))
+;(custom-set-variables
+; '(py-python-command "c:\\python26\\python.exe"))
 (custom-set-faces)
+
+(defun ropemacs-start ()
+  "Loads pymacs and ropemacs"
+  (interactive)
+  (require 'pymacs)
+  (pymacs-load "ropemacs" "rope-")
+  ;; Automatically save project python buffers before refactorings
+  (setq ropemacs-confirm-saving 'nil))
+
+(defun pylons-start ()
+  (interactive)
+  (set (make-local-variable 'python-command) "c:\\pylons\\Scripts\\python.exe"))
